c3c241e8d26a8c21c3cbee14ab25da7e794f59d4
Jarek <jarek@jarek-mac.local>
2018-03-23 08:24:44 +0100

    Initial Commit

---
+++ See Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  See Food
+//
+//  Created by Jarek on 23/03/2018.
+//  Copyright © 2018 Jarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
